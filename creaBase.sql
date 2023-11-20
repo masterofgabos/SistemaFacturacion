db70151c7b255acd8d52231be17d07ec00bf91f2
masterofgabos <masterofgabos9719@gmail.com>
2023-11-20 18:32:15 -0300

    Modificacion Script BD

--- creaBase.sql
+++ creaBase.sql
@@ -4,6 +4,9 @@ CREATE TABLE Usuario(
 usuario_correo  VARCHAR (150) NOT NULL,
 usuario_contraseña VARCHAR (100) NOT NULL);
 
+
+/*Crear tabla desde 0*/
+
 CREATE TABLE OrdenCompra(
 id_oc				INT NOT NULL IDENTITY (1,1),
 cliente_oc			VARCHAR (100) NOT NULL,
@@ -13,7 +16,7 @@ descripcion_oc		VARCHAR (150) NOT NULL,
 producto_oc			VARCHAR (150) NOT NULL,
 monto_oc			INT NOT NULL,
 descuentos_oc		INT NOT NULL,
-monto_total_oc		INT NOT NULL,
+monto_total_oc		INT,
 fecha_oc			DATE NOT NULL,
 rut_cli_oc			VARCHAR (15) NOT NULL,
 rut_prov_oc			VARCHAR (15) NOT NULL,
@@ -22,6 +25,14 @@ tel_prov_oc			INT NOT NULL,
 correo_cli_oc		VARCHAR (150) NOT NULL,
 correo_prov_oc		VARCHAR (150) NOT NULL,
 dir_cli_oc			VARCHAR (150) NOT NULL,
-dir_prov_oc			VARCHAR (150) NOT NULL);
+dir_prov_oc			VARCHAR (150) NOT NULL,
+despacho_oc         VARCHAR (20) NOT NULL;
 
 ALTER TABLE OrdenCompra ADD CONSTRAINT PK_OC PRIMARY KEY (id_oc);
+
+
+
+/*En caso de querer modificar la tabla ya existente*/
+
+ALTER TABLE OrdenCompra ADD despacho_oc VARCHAR(20) NOT NULL;
+ALTER TABLE OrdenCompra ALTER COLUMN monto_total_oc INT;
