a9521fe51d98e20b760c6434428a2340717196e4
masterofgabos <masterofgabos9719@gmail.com>
2023-12-11 04:37:46 -0300

    Version Final
    RF 6 y 7

--- OrdenesCompra/FormularioVerOC.Designer.cs
+++ OrdenesCompra/FormularioVerOC.Designer.cs
@@ -28,52 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.DgvProducto = new System.Windows.Forms.DataGridView();
             this.salir = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.buscar = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.btnCambiarEstado = new System.Windows.Forms.Button();
+            this.btnbuscarfecha = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.btnExportar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.DgvProducto)).BeginInit();
             this.SuspendLayout();
             // 
             // DgvProducto
             // 
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.DgvProducto.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgvProducto.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.DgvProducto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.DgvProducto.DefaultCellStyle = dataGridViewCellStyle2;
-            this.DgvProducto.Location = new System.Drawing.Point(58, 152);
-            this.DgvProducto.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DgvProducto.DefaultCellStyle = dataGridViewCellStyle4;
+            this.DgvProducto.Location = new System.Drawing.Point(29, 111);
+            this.DgvProducto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DgvProducto.Name = "DgvProducto";
             this.DgvProducto.RowHeadersWidth = 51;
             this.DgvProducto.RowTemplate.Height = 24;
-            this.DgvProducto.Size = new System.Drawing.Size(1410, 746);
+            this.DgvProducto.Size = new System.Drawing.Size(705, 356);
             this.DgvProducto.TabIndex = 30;
             this.DgvProducto.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvProducto_CellContentClick);
             // 
             // salir
             // 
             this.salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.salir.Location = new System.Drawing.Point(58, 927);
-            this.salir.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.salir.Location = new System.Drawing.Point(29, 482);
+            this.salir.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.salir.Name = "salir";
-            this.salir.Size = new System.Drawing.Size(1410, 67);
+            this.salir.Size = new System.Drawing.Size(705, 35);
             this.salir.TabIndex = 31;
             this.salir.Text = "Volver al menu";
             this.salir.UseVisualStyleBackColor = true;
@@ -82,22 +85,22 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.label1.Location = new System.Drawing.Point(966, 60);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(530, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(502, 63);
+            this.label1.Size = new System.Drawing.Size(204, 26);
             this.label1.TabIndex = 33;
             this.label1.Text = "Ordenes de compra";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // buscar
             // 
-            this.buscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.buscar.Location = new System.Drawing.Point(289, 75);
-            this.buscar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.buscar.Location = new System.Drawing.Point(144, 42);
+            this.buscar.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.buscar.Name = "buscar";
-            this.buscar.Size = new System.Drawing.Size(135, 47);
+            this.buscar.Size = new System.Drawing.Size(163, 24);
             this.buscar.TabIndex = 34;
             this.buscar.Text = "Buscar";
             this.buscar.UseVisualStyleBackColor = true;
@@ -106,29 +109,66 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.textBox1.Location = new System.Drawing.Point(58, 77);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Location = new System.Drawing.Point(29, 40);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(214, 44);
+            this.textBox1.Size = new System.Drawing.Size(109, 26);
             this.textBox1.TabIndex = 35;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // btnCambiarEstado
             // 
-            this.btnCambiarEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCambiarEstado.Location = new System.Drawing.Point(431, 75);
+            this.btnCambiarEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnCambiarEstado.Location = new System.Drawing.Point(311, 41);
+            this.btnCambiarEstado.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCambiarEstado.Name = "btnCambiarEstado";
-            this.btnCambiarEstado.Size = new System.Drawing.Size(411, 47);
+            this.btnCambiarEstado.Size = new System.Drawing.Size(423, 24);
             this.btnCambiarEstado.TabIndex = 36;
-            this.btnCambiarEstado.Text = "Cambiar Estado Despacho";
+            this.btnCambiarEstado.Text = "Cambiar Estado Despacho ID Seleccionado";
             this.btnCambiarEstado.UseVisualStyleBackColor = true;
             this.btnCambiarEstado.Click += new System.EventHandler(this.btnCambiarEstado_Click);
             // 
+            // btnbuscarfecha
+            // 
+            this.btnbuscarfecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnbuscarfecha.Location = new System.Drawing.Point(144, 80);
+            this.btnbuscarfecha.Name = "btnbuscarfecha";
+            this.btnbuscarfecha.Size = new System.Drawing.Size(163, 23);
+            this.btnbuscarfecha.TabIndex = 39;
+            this.btnbuscarfecha.Text = "Buscar por Fecha";
+            this.btnbuscarfecha.UseVisualStyleBackColor = true;
+            this.btnbuscarfecha.Click += new System.EventHandler(this.btnbuscarfecha_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.maskedTextBox1.Location = new System.Drawing.Point(29, 77);
+            this.maskedTextBox1.Mask = "00/00/0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(109, 26);
+            this.maskedTextBox1.TabIndex = 40;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnExportar.Location = new System.Drawing.Point(313, 80);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(421, 23);
+            this.btnExportar.TabIndex = 41;
+            this.btnExportar.Text = "Exportar datos DataGridView a Excel";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
             // FormularioVerOC
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1520, 1033);
+            this.ClientSize = new System.Drawing.Size(760, 537);
+            this.Controls.Add(this.btnExportar);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.btnbuscarfecha);
             this.Controls.Add(this.btnCambiarEstado);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.buscar);
@@ -136,9 +176,9 @@
             this.Controls.Add(this.salir);
             this.Controls.Add(this.DgvProducto);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "FormularioVerOC";
             this.Text = "FormularioVerOC";
+            this.Load += new System.EventHandler(this.FormularioVerOC_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DgvProducto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -153,5 +193,8 @@
         private System.Windows.Forms.Button buscar;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btnCambiarEstado;
+        private System.Windows.Forms.Button btnbuscarfecha;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button btnExportar;
     }
 }
